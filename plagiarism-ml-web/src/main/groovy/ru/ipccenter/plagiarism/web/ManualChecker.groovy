b38a57fa428a8cbcd91f2c3b32bf730373b8aa7d
Dmitry Kholodilov <dmitry.kholodilov@gmail.com>
2013-03-27 01:29:38 +0400

    fix build

--- plagiarism-ml-web/src/main/groovy/ru/ipccenter/plagiarism/web/ManualChecker.groovy
+++ plagiarism-ml-web/src/main/groovy/ru/ipccenter/plagiarism/web/ManualChecker.groovy
@@ -6,13 +6,12 @@ import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
-import javax.ws.rs.core.MediaType;
 
 /**
  * @author dmitry
  */
 @Path("/")
-@Produces(MediaType.TEXT_HTML)
+@Produces("text/html")
 class ManualChecker
 {
 
