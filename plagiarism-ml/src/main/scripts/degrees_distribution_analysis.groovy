f27e8c9e3fbe86c4d5519d21822d5d8ec90d94f3
Dmitry Kholodilov <dmitry.kholodilov@gmail.com>
2013-06-09 17:00:47 +0400

    changed reporting in degrres distr analysis

--- plagiarism-ml/src/main/scripts/degrees_distribution_analysis.groovy
+++ plagiarism-ml/src/main/scripts/degrees_distribution_analysis.groovy
@@ -5,7 +5,7 @@ import ru.ipccenter.plagiarism.solutions.impl.ManualChecksSolutionsPairRepositor
 import ru.ipccenter.plagiarism.solutions.impl.SolutionRepositoryFSImpl
 import ru.ipccenter.plagiarism.solutions.impl.TaskRepositoryFileImpl
 
-import static ru.ipccenter.plagiarism.detectors.impl.PlaggieAdaptiveMode.fuzzySubsequenceOrReverseSubsequence
+import static ru.ipccenter.plagiarism.detectors.impl.PlaggieAdaptiveMode.diffOnlyFuzzySubsequenceOrReverseSubsequence
 
 def dataDirectoryPath = args[0]
 
@@ -14,40 +14,69 @@ def solutionRepository = new SolutionRepositoryFSImpl(dataDirectoryPath)
 def learningSolutionsPairRepository = new ManualChecksSolutionsPairRepository(solutionRepository, dataDirectoryPath)
 def allSolutionsPairRepository = new AllSolutionsPairRepository(solutionRepository)
 
-def TASK = "collections2"
+def ALLOWED_MANUAL = 25
+
+/*def TASK = "reflection0"
 def LEARNING_GROUPS = ["L1", "L2"]
 def ADAPTIVE_MODE = fuzzySubsequenceOrReverseSubsequence(5)
+def MML_FOR_PLAIN = 11
+def MML_FOR_ADAPTIVE = 9..11*/
+
+/*
+def TASK = "collections2"
+def LEARNING_GROUPS = ["L1", "L2"]
+def ADAPTIVE_MODE = fuzzySubsequenceOrReverseSubsequence(4)
+def MML_FOR_PLAIN = 10
+def MML_FOR_ADAPTIVE = 9..10
+*/
+
+def TASK = "array1"
+def LEARNING_GROUPS = ["L1", "L2"]
+def ADAPTIVE_MODE = diffOnlyFuzzySubsequenceOrReverseSubsequence(4, 3) // fuzzySubsequenceOrReverseSubsequence(3)
+def MML_FOR_PLAIN = 11
+def MML_FOR_ADAPTIVE = 8..11
+
+def plainDetector = new PlaggieDetector(MML_FOR_PLAIN)
 
 [taskRepository.find(TASK)].each { task ->
 
+    println "#### $task"
+
     def allPairs = allSolutionsPairRepository.findFor(task)
 
-    (8..11).each { minMatchLength ->
+    def detectionResults = allPairs.collect { plainDetector.performDetection(it) }
 
-        println "#### $task, minMatchLength=$minMatchLength"
+    def originalDegreesDistribution =
+        detectionResults.collect { it.similarityDegree.value }
+                .inject([:].withDefault {0}) { map, value ->  map[value]++; map }
+                .sort()
 
-        def plainDetector = new PlaggieDetector(minMatchLength)
+    print "Original distribution: $originalDegreesDistribution (MML=$MML_FOR_PLAIN), "
 
-        def originalDegreesDistribution =
-                    allPairs.collect { plainDetector.performDetection(it).similarityDegree.value }
-                            .inject([:].withDefault {0}) { map, value ->  map[value]++; map }
-                            .sort()
+    println "Similarity threshold " + detectionResults.sort { -it.similarity }.take(ALLOWED_MANUAL).last().similarity
 
-        println "Original distribution: $originalDegreesDistribution"
+    MML_FOR_ADAPTIVE.each { minMatchLength ->
 
         LEARNING_GROUPS.each { learningGroup ->
 
-            def learningPairs = learningSolutionsPairRepository.findFor(task, learningGroup)
+            def learningPairs = learningSolutionsPairRepository.findFor(task, learningGroup).take(10)
 
             def detector = new PlaggieAdaptiveDetector(minMatchLength, ADAPTIVE_MODE)
             detector.learnOnPairsWithZeroEstimatedSimilarity(learningPairs)
 
+            def correctedDetectionResults = allPairs.collect { detector.performDetection(it) }
+
             def correctedDegreesDistribution =
-                        allPairs.collect { detector.performDetection(it).correctedSimilarityDegree.value }
+                correctedDetectionResults.collect { it.correctedSimilarityDegree.value }
                                 .inject([:].withDefault {0}) { map, value ->  map[value]++; map }
                                 .sort()
 
-            println "$learningGroup: $correctedDegreesDistribution"
+            print "$learningGroup: $correctedDegreesDistribution (MML=$minMatchLength), "
+
+            println "Similarity threshold " +
+                    correctedDetectionResults.sort { -it.correctedSimilarity }
+                                    .take(ALLOWED_MANUAL - learningPairs.size()).last().correctedSimilarity
+
 
         }
 
